dab307470ec1f7ef2a6f068dbda0914e80d8aa52
Wioleta Jemieljanczuk <W.Jemieljanczuk@agrovision.com>
2021-03-04 07:45:32 +0100

    Add project files.

---
+++ APIVersioning/Middleware/Middleware.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace APIVersioning.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.StartsWithSegments("/api") && !ContainsVersioningComponent(httpContext.Request))
+            {
+                var clientType = ClientType(httpContext.Request.Headers);
+
+                httpContext.Request.Path = RewritePath(clientType, httpContext.Request.Headers);
+            }
+            return _next(httpContext);
+        }
+
+        private string ClientType(IHeaderDictionary headers)
+        {
+            return "MOBILE";
+        }
+
+        private PathString RewritePath(string clientType, IHeaderDictionary headers)
+        {
+            if (clientType == "MOBILE")
+            {
+                var appVersion = ExtractAppVersion(headers);
+                if (appVersion >= 2)
+                {
+                    return "/api/v2/WeatherForecast";
+                }
+            }
+            return "/api/v1/WeatherForecast";
+        }
+
+        private bool ContainsVersioningComponent(HttpRequest request)
+        {
+            var path = request.Path;
+            var v1 = path.StartsWithSegments(new PathString("/api/v1"), StringComparison.OrdinalIgnoreCase);
+            var v2 = path.StartsWithSegments(new PathString("/api/v2"), StringComparison.OrdinalIgnoreCase);
+
+            return v1 || v2;
+        }
+
+        private int? ExtractAppVersion(IHeaderDictionary headers)
+        {
+            return 4;
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
---
+++ APIVersioning/Startup.cs
@@ -0,0 +1,50 @@
+using APIVersioning.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace APIVersioning
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning(config =>
+            {
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.ReportApiVersions = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware();
+
+            app.UseApiVersioning();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
